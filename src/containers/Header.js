ae3cb3607bda75e8591a49ae65a698440c335ead
Neeraj <nxxwxj@gmail.com>
2022-03-31 00:15:41 +0530

    added the Header component

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Navigation, Pagination, Autoplay } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/autoplay'
+import { warehouseSlide, cover3Slide, shipyardSlide } from '../assets'
+
+const Image = ({ src }) => {
+  return (
+    <div class="h-[650px] sm:h-[550px] ">
+      <img src={src} class='object-cover w-full h-full' />
+      {/* <div class='w-full h-full bg-gray-200 opacity-25'/> */}
+    </div>
+  )
+}
 
 const Header = () => {
   return (
-    <div class='mt-28'>Header</div>
+    <div class="mt-24 mb-10">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation
+        autoplay={{ delay: 3000 }}
+        pagination={{ clickable: true }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <Image src={warehouseSlide} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={cover3Slide} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={shipyardSlide} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   )
 }
 
